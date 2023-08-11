430424676f928c501d6332d65e43a888d5152d6d
ovibaridar <ovibaridar06@gmail.com>
2023-08-11 23:46:22 +0600

    concatenate

---
+++ concatination.py
@@ -0,0 +1,7 @@
+import numpy as np
+vr = np.array([[1, 1, 2, 3], [3, 4, 4, 5]])
+vrs = np.array([[12, 12, 22, 32], [32, 42, 42, 52]])
+print(vr, "\n")
+print(vrs, "\n")
+concat = np.concatenate((vr, vrs), axis=1)
+print(concat, "\n")
