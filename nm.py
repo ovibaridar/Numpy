28c84ae90ccb927db9982c4c19eddc4467357217
ovibaridar <ovibaridar06@gmail.com>
2023-08-11 17:22:57 +0600

    copy and view

--- nm.py
+++ nm.py
@@ -1,19 +1,31 @@
 import numpy as np
 
-# vr = np.linspace(1, 100, 5,dtype=int)
+# vr = np.linspace(1, 100, 5, dtype=int)
 # vr = np.arange(1, 10, 5, dtype=float)
 # vr = np.logspace(10, 20, 5, dtype=int)
 # vr = np.zeros((3, 3))
 # vr = np.ones((3, 3))
 # value = 5
 # vr = np.full((3, 3), value)
-vr = np.eye(3)
+vr = np.arange(1, 10, 1)
 
 # reflections of vr and the id is different
 # vrs = vr.view()
 
 # copy of vr and the id is Same
-vrs = vr
-
+# vrs = vr
 print(id(vr))
-print(id(vrs))
+
+# vrs = vr.view()
+# print(vrs)
+# print(id(vrs))
+# vrs[3] = 400
+# print(vrs, "vrs after change")
+# print(vr, "vr after change")
+
+# vrs = vr.copy()
+# print(vrs)
+# print(id(vrs))
+# vrs[3] = 400
+# print(vrs, "vrs after change")
+# print(vr, "vr after change")
