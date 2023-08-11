7072d5e3f6bf0ff18efb625de8152b633e02053c
ovibaridar <ovibaridar06@gmail.com>
2023-08-11 22:59:42 +0600

    new function

--- nm.py
+++ nm.py
@@ -7,14 +7,14 @@ import numpy as np
 # vr = np.ones((3, 3))
 # value = 5
 # vr = np.full((3, 3), value)
-vr = np.arange(1, 10, 1)
+# vr = np.arange(1, 10, 1)
 
 # reflections of vr and the id is different
 # vrs = vr.view()
 
 # copy of vr and the id is Same
 # vrs = vr
-print(id(vr))
+# print(id(vr))
 
 # vrs = vr.view()
 # print(vrs)
@@ -24,8 +24,28 @@ print(id(vr))
 # print(vr, "vr after change")
 
 # vrs = vr.copy()
+# print(vrs, "vrs\n")
+# print(vr, "vr\n")
+# print(id(vrs), "vrs\n")
+# print(id(vr), "vr\n")
+# vrs[2] = 200
+# print(vrs, "vrs\n")
+# print(vr, "vr\n")
+
+# vr = np.array([[1, 2, 3], [3, 4, 5]])
+# vrs = np.array([[12, 21, 33], [34, 45, 56]])
+# print(vr)
 # print(vrs)
-# print(id(vrs))
-# vrs[3] = 400
-# print(vrs, "vrs after change")
-# print(vr, "vr after change")
+#
+# vradd = np.add(vr, vrs)
+# vrasub = np.subtract(vrs, vr)
+# vramultiplication = np.multiply(vrs, vr)
+# vramod = np.mod(vrs, 2)
+# vrareciprocal = np.reciprocal(vr)
+#
+#
+# print("Numpy add\n", vradd)
+# print("Numpy subtract\n", vrasub)
+# print("Numpy multiply\n", vramultiplication)
+# print("Numpy MOD\n", vramod)
+# print("Numpy reciprocal\n", vrareciprocal)
