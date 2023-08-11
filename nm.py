fe038d75e10b828c50ed331d0a4a1a6e228fb49b
ovibaridar <ovibaridar06@gmail.com>
2023-08-11 16:54:42 +0600

    functions

---
+++ nm.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# vr = np.linspace(1, 100, 5,dtype=int)
+# vr = np.arange(1, 10, 5, dtype=float)
+# vr = np.logspace(10, 20, 5, dtype=int)
+# vr = np.zeros((3, 3))
+# vr = np.ones((3, 3))
+# value = 5
+# vr = np.full((3, 3), value)
+vr = np.eye(3)
+
+print(vr)
