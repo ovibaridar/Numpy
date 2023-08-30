2b5905e714675b74a7e4a9e30885661213744a9a
Ovi <ovibaridar06@gmail.com>
2023-08-30 22:37:48 +0600

    new

--- split.py
+++ split.py
@@ -1,5 +1,10 @@
-import numpy  as  nm
-vr=nm.arange(1,10,1)
+import numpy as nm
+
+vr = nm.arange(1, 10, 1)
 print(vr)
-arr=nm.split(vr,3)
+arr = nm.split(vr, 3)
 print(arr)
+
+vr = nm.arange(1, 10, 2)
+
+print(vr)
